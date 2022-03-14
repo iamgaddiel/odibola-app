81330831698da360d353044c23bcb276ce611d4d
iamgaddiel <ighotagaddiel98@gmail.com>
2022-03-14 19:35:59 -0400

    finshed last screens

---
+++ src/components/BackHeaderWithTitle.tsx
@@ -0,0 +1,24 @@
+import { IonBackButton, IonButtons, IonHeader, IonTitle, IonToolbar } from '@ionic/react'
+import React from 'react'
+
+
+interface props {
+    backLink: string
+    title: string
+}
+
+const BackHeaderWithTitle: React.FC<props> = ({ backLink, title }: props) => {
+    return (
+        <IonHeader className='ion-no-border'>
+            <IonToolbar>
+                <IonButtons slot='start'>
+                    <IonBackButton text='Back' defaultHref={backLink} />
+                </IonButtons>
+
+                <IonTitle>{title}</IonTitle>
+            </IonToolbar>
+        </IonHeader>
+    )
+}
+
+export default BackHeaderWithTitle
